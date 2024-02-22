ed56f83feeb5a1d6410e9a90d3bd3423ae287a47
govardhan-26 <bgrnaidu@gmail.com>
2024-02-23 01:36:17 +0530

    signin page

--- Client/src/components/pages/Dashboard.jsx
+++ Client/src/components/pages/Dashboard.jsx
@@ -8,11 +8,11 @@ const Dashboard = () => {
         <div className='welcome'>
             <div className='welcome-content'>
                         <div className='pic-bg'></div>
-                {/* <div className='welcome-pic'> */}
-                        <img src={signin} className='wel-pic' alt="pic" />
-                {/* </div> */}
+                <div className='welcome-pic'>
+                    <img src={signin} className='wel-pic' alt="pic" />
+                </div>
                 <div className='welcome-text'>
-
+                    <h1></h1>
                 </div>
             </div>
         </div>
