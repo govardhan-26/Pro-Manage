a79deaa62591c22a0576c8b7a8412d271ff5ec7e
govardhan-26 <bgrnaidu@gmail.com>
2024-02-24 16:03:39 +0530

    hello

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
-import Dashboard from "./pages/Dashboard/Dashboard";
+
 function App() {
   return (
     <BrowserRouter>
