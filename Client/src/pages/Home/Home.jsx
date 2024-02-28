a1ece900e19a2eb6d41d1933074db66335938135
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-02-29 00:23:04 +0530

    input field update

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { DisplayCard } from '../../components'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 const Home = () => {
   return (
-    <div style={{
-      margin:"50px"
-    }}>
-      <DisplayCard/>
+    <div
+      style={{
+        margin: '50px',
+      }}
+    >
+      <Calendar />
     </div>
   )
 }
