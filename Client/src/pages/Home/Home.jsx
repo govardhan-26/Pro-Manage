1944d668aa48d3af4b33a68bdc4512b5ac1b0018
govardhan-26 <bgrnaidu@gmail.com>
2024-02-28 17:31:29 +0530

    small alterings

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,66 +1,70 @@
-import { Badge, DateBadge, Ellipse, LoginInput, RedDateBadge, TaskInput, Taskholder,Taskshow } from '../../components'
-import './Home.css'
-import dots from '../../assets/three-dots.svg'
-import dropup from '../../assets/drop-up.svg'
-import dropdown from '../../assets/drop-down.svg'
-import { useState } from 'react'
-
-
+import {
+  Badge,
+  DateBadge,
+  Ellipse,
+  LoginInput,
+  RedDateBadge,
+  TaskInput,
+  Taskholder,
+  Taskshow,
+} from "../../components";
+import "./Home.css";
+import dots from "../../assets/three-dots.svg";
+import dropup from "../../assets/drop-up.svg";
+import dropdown from "../../assets/drop-down.svg";
+import { useState } from "react";
 
 const Home = () => {
+  const [drop, setDrop] = useState(true);
 
-  const [drop, setDrop] = useState(true); 
+  const array = [1, 2, 3];
 
-  const array = [1,2,3];
-
-  const handleDrop = () =>{
-    if(drop)setDrop(false);
+  const handleDrop = () => {
+    if (drop) setDrop(false);
     else setDrop(true);
-  }
+  };
 
   return (
-    <div style={{margin : "50px"}}>
-    <div className='todo-card-container'>
-      <div className='todo-card-content'>
-        <div className='todo-card-priority'>
-          <div className='todo-card-prior'>
-            <Ellipse bg = "green"/>
-            <div className='todo-card-priority-text'>HIGH PRIORITY</div>
+    <div className="todo-card-container">
+      <div className="todo-card-content">
+        <div className="todo-card-priority">
+          <div className="todo-card-prior">
+            <Ellipse bg="green" />
+            <div className="todo-card-priority-text">HIGH PRIORITY</div>
           </div>
-          <div className='todo-cards-dots'>
+          <div className="todo-cards-dots">
             <img src={dots} alt="dots" />
           </div>
         </div>
-        <div className='todo-card-heading'>
-          <div className='todo-card-heading-text'>
-            Hero Section  
-          </div>
+        <div className="todo-card-heading">
+          <div className="todo-card-heading-text">Hero Section</div>
         </div>
         <div className="todo-card-checklist">
-          <div className='todo-checklist-heading'>
+          <div className="todo-checklist-heading">
             <h2>Checklist (0/3)</h2>
-            <div className='down-icon' onClick={handleDrop}>
+            <div className="down-icon" onClick={handleDrop}>
               {drop ? (
                 <img src={dropup} alt="dropup" />
-                ) :(
+              ) : (
                 <img src={dropdown} alt="dropdown" />
-              )
-              }
+              )}
             </div>
           </div>
-          {drop ? (<div className='todo-checklist-content'>
-            {
-              array.map((item, index) => {
-                  return (<Taskshow/>)
-              })
-            }
-          </div>) : (<></>)}
+          {drop ? (
+            <div className="todo-checklist-content">
+              {array.map((item, index) => {
+                return <Taskshow />;
+              })}
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
         <div className="todo-card-progress">
-          <div className='todo-card-progress-1'>
+          <div className="todo-card-progress-1">
             <RedDateBadge>Feb 10th</RedDateBadge>
           </div>
-          <div className='todo-card-progress-2'>
+          <div className="todo-card-progress-2">
             <Badge width="42px">PROGRESS</Badge>
             <Badge width="42px">TODO</Badge>
             <Badge width="42px">DONE</Badge>
@@ -68,8 +72,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
